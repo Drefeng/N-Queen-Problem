c1015f94ae62f4415bdbacdc4151ceb4ee93268e
Billy Infante <infbil15@gmail.com>
2019-02-15 11:52:38 +0100

    board print

---
+++ Board.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Board{
+
+    private Piece[][] board; 
+     
+    public Board(int size){
+         board = new Piece[size][size];
+    
+    }
+
+
+
+    public void drawBoard(){
+
+        for(int x=0; x < board.length; x++){
+            for(int y=0; y < board.length; y++){
+                if(board[x][y] != null){
+                    System.out.print(board[x][y]);
+                }
+                else{
+                    System.out.print("| O");
+                }
+            }
+        }
+    System.out.println();
+    }
+
+}
